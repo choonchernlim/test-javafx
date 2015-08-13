7c11c4976235324144b1e31cca1b2aa4329b41a3
Choon-Chern Lim <limc@mayo.edu>
2015-08-13 15:54:43 -0500

    Initial

---
+++ src/main/java/project/testJavaFx/FXMLExampleController.java
@@ -0,0 +1,18 @@
+package project.testJavaFx;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.PasswordField;
+import javafx.scene.text.Text;
+
+public class FXMLExampleController {
+    public PasswordField passwordField;
+
+    @FXML
+    private Text actiontarget;
+
+    @FXML
+    protected void handleSubmitButtonAction(ActionEvent event) {
+        actiontarget.setText("Sign in button pressed");
+    }
+}
